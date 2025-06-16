cc31522a8a37a62637dce4f682f52a4dbfbc5638
delroscol98 <collindelrosario98@gmail.com>
2025-06-16 12:52:48 +1000

    Revert "More graph algos"
    This reverts commit 9d26ce7512ab8aed6adae9a2dd105c9d5161949a.

--- Graphs/Graphs.js
+++ Graphs/Graphs.js
@@ -1,45 +1,55 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
+const { getUser } = require("./jobs");
+
+const findMostCommonTitle = (myId, degreesOfSeparation) => {
+  let queue = [myId];
+  const seen = new Set(queue);
+  const jobs = {};
+
+  for (let i = 0; i <= degreesOfSeparation; i++) {
+    const newQueue = [];
+    while (queue.length) {
+      const user = getUser(queue.shift());
+
+      // queue up next iteration of connections
+      for (let j = 0; j < user.connections.length; j++) {
+        const connection = user.connections[j];
+        if (!seen.has(connection)) {
+          newQueue.push(connection);
+          seen.add(connection);
+        }
+      }
+
+      // count the job
+      jobs[user.title] = jobs[user.title] ? jobs[user.title] + 1 : 1;
+    }
+
+    queue = newQueue;
   }
 
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
-      return true;
+  // find key with the biggest number
+
+  const jobKeys = Object.keys(jobs);
+
+  let biggestNumber = jobs[jobKeys[0]];
+  let jobName = jobKeys[0];
+  for (let i = 1; i < jobKeys.length; i++) {
+    const currentJob = jobKeys[i];
+    if (jobs[currentJob] > biggestNumber) {
+      jobName = currentJob;
+      biggestNumber = jobs[currentJob];
     }
-    return false;
   }
 
-  addEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1].push(vertex2);
-      this.adjacencyList[vertex2].push(vertex1);
-      return true;
-    }
-    return false;
-  }
+  //   see all job titles, sorted
+  jobKeys
+    .map((id) => [id, jobs[id]])
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .forEach(([job, num]) => console.log(`${num} – ${job}`));
 
-  removeEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-        (v) => v !== vertex2
-      );
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-        (v) => v !== vertex1
-      );
-      return true;
-    }
-    return false;
-  }
+  console.log("======");
 
-  removeVertex(vertex) {
-    if (!this.adjacencyList[vertex]) return undefined;
-    while (this.adjacencyList[vertex].length) {
-      let temp = this.adjacencyList[vertex].pop();
-      this.removeEdge(vertext, temp);
-    }
-    delete this.adjacencyList[vertex];
-    return this;
-  }
-}
+  return jobName;
+};
+
+console.log(findMostCommonTitle(30, 2));
--- Trees/BFS.js
+++ Trees/BFS.js
@@ -2,9 +2,12 @@ function BFS(head, needle) {
   const q = [head];
   while (q.length) {
     let curr = q.shift();
-    if (!curr) continue;
-    if (curr.value === needle) return true;
-
+    if (!curr) {
+      continue;
+    }
+    if (curr.value === needle) {
+      return true;
+    }
     q.push(curr.left);
     q.push(curr.right);
   }
