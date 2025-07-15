52e45b8ec243ca0a5dc2a8a910a29776ebbce85f
delroscol98 <collindelrosario98@gmail.com>
2025-07-15 21:47:31 +1000

    small fixes

--- Sorts/insertionSort.js
+++ Sorts/insertionSort.js
@@ -2,8 +2,8 @@ export function insertionSort(nums) {
   for (let i = 1; i < nums.length; i++) {
     let temp = nums[1];
     let j;
-    for (j = i - 1; nums[j] > temp && j >= -1; j--) {
-      nums[j + 1] = temp;
+    for (j = i - 1; nums[j] > temp && j > -1; j--) {
+      nums[j + 1] = nums[j];
     }
     nums[j + 1] = temp;
   }
